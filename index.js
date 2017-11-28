1523df7ed7be8d98fcd88845102302c524194d66
Joseph Karp <joe12345hd@yahoo.com>
2017-11-28 18:13:45 -0500

    init
    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views')); // specify the views directory
+app.set('view engine', 'pug'); // register the template engine
+
+app.use(express.static(path.join(__dirname, 'public')));//lets the front end get resources
+
+app.get('/',function(req,res)
+{
+    res.render('index');
+});
+
+app.use(function(err,req,res,next)
+{
+    res.send(err.toString());
+});
+
+app.listen('80');
